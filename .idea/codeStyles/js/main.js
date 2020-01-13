9cccb6b145d112ea98f58d37fa8691360edebc0c
김경록 <goodboy3421@gmail.com>
2020-01-13 16:01:33 +0900

    비동기 이벤트를 이용한 Hello! Node.js! 출력

--- .idea/codeStyles/js/main.js
+++ .idea/codeStyles/js/main.js
@@ -1,10 +1,23 @@
 //  main.js
 
-var fs = require("fs");
+// var fs = require("fs");
+//
+// fs.readFile('./hello.txt', encoding = 'utf-8', function(err, data) {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log(data + " Node.js!");
+// });
 
-fs.readFile('./hello.txt', encoding = 'utf-8', function(err, data) {
-    if (err) {
-        throw err;
-    }
-    console.log(data + " Node.js!");
+// main.js
+
+var EventEmitter = require("events").EventEmitter;
+var evt = new EventEmitter();
+
+evt.on("helloNode", function(str) {
+    console.log("Hello! " + str );
 });
+
+setTimeout(function() {
+    evt.emit("helloNode", "Node.js");
+}, 3000);
